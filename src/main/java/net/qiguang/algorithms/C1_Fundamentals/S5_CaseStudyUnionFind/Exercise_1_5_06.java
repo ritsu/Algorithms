e0b600cb4c0c38e657675b1ef9e2e602684493d1
Ritsu <ritsu@users.noreply.github.com>
2017-09-11 20:38:51 -0400

    Add exercises 1.5.05 - 1.5.06

---
+++ src/main/java/net/qiguang/algorithms/C1_Fundamentals/S5_CaseStudyUnionFind/Exercise_1_5_06.java
@@ -0,0 +1,18 @@
+package net.qiguang.algorithms.C1_Fundamentals.S5_CaseStudyUnionFind;
+
+/**
+ * 1.5.6
+ * Repeat Exercise 1.5.5 for weighted quick-union.
+ *
+ *    Number of iterations per union ~ 2 * lg(10^6)
+ *                  Number of unions ~ 10^6        (assume most input pairs result in a union)
+ *        Number of union iterations ~ 2 * 10^6 * lg(10^6)
+ *      Number of union instructions ~ 2 * 10^7 * lg(10^6)
+ *
+ *           Total number of seconds ~ 2 * 10^7 * lg(10^6) / 10^9
+ *                                   ~ 2 * lg(10^6) / 10^2
+ *                                   ~ 40 / 100 = 0.4 seconds
+ */
+public class Exercise_1_5_06 {
+    // Nothing to see here.
+}
