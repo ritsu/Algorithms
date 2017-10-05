0e13da50067fd6be6d85d75f9cb9a232baba0eed
Ritsu <ritsu@users.noreply.github.com>
2017-10-04 21:09:14 -0400

    Add exercises 2.4.05 - 2.4.08

---
+++ src/main/java/net/qiguang/algorithms/C2_Sorting/S4_PriorityQueues/Exercise_2_4_07.java
@@ -0,0 +1,15 @@
+package net.qiguang.algorithms.C2_Sorting.S4_PriorityQueues;
+
+/**
+ * 2.4.7 The largest item in a heap must appear in position 1, and the second largest must be in
+ * position 2 or position 3. Give the list of positions in a heap of size 31 where the kth largest
+ * (i) can appear, and (ii) cannot appear, for k=2, 3, 4 (assuming the values to be distinct).
+ *
+ * Answer:
+ *   k  (i)      (ii)
+ *   2  2,3      all others
+ *   3  2,3      all others
+ *   4  4,5,6,7  all others
+ */
+public class Exercise_2_4_07 {
+}
