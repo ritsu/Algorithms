56c26be772df63e738597b224798f9ab80f648a0
Ritsu <ritsu@users.noreply.github.com>
2017-10-07 10:00:40 -0400

    Update 2.4.09

--- src/main/java/net/qiguang/algorithms/C2_Sorting/S4_PriorityQueues/Exercise_2_4_09.java
+++ src/main/java/net/qiguang/algorithms/C2_Sorting/S4_PriorityQueues/Exercise_2_4_09.java
@@ -6,8 +6,8 @@ package net.qiguang.algorithms.C2_Sorting.S4_PriorityQueues;
  *
  * Answer
  * assume min heap:
- * 0 1 2 3 4 5      (4, 5 are children of 2)
- *   A B C D E
+ * 0 1 2 3 4 5 6 7    (4, 5 are children of 2)
+ *   A B C D E        (6, 7 are children of 3)
  *   A B C E D
  *   A B D C E
  *   A B D E C
@@ -15,11 +15,31 @@ package net.qiguang.algorithms.C2_Sorting.S4_PriorityQueues;
  *   A B E D C
  *   A C B D E
  *   A C B E D
+ *   A C B     D E
+ *   A C B     E D
+ *   A D B     C E
+ *   A D B     E C
+ *   A E B     C D
+ *   A E B     D C
+ *   A B C     D E
+ *   A B C     E D
+ *   A B C D   E
+ *   A B C E   D
+ *   A C B E   D
+ *   A C B D   E
+ *   A B D C   E
+ *   A D B E   C
  *
- * 0 1 2 3 4 5
+ * 0 1 2 3 4 5 6 7
  *   A A A B B
  *   A A B A B
  *   A A B B A
+ *   A A A     B A
+ *   A B A     A B
+ *   A B A     B A
+ *   A A A B   B
+ *   A A B A   B
+ *   A B A B   A
  */
 public class Exercise_2_4_09 {
 }
