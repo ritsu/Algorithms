b07ad60a8d62dc776b366046ba735af187924374
Ritsu <ritsu@users.noreply.github.com>
2017-10-07 08:59:10 -0400

    Add exercises 2.4.09 - 2.4.12

---
+++ src/main/java/net/qiguang/algorithms/C2_Sorting/S4_PriorityQueues/Exercise_2_4_09.java
@@ -0,0 +1,25 @@
+package net.qiguang.algorithms.C2_Sorting.S4_PriorityQueues;
+
+/**
+ * 2.4.9 Draw all of the different heaps that can be made from the ﬁve keys A B C D E,
+ * then draw all of the different heaps that can be made from the ﬁve keys A A A B B.
+ *
+ * Answer
+ * assume min heap:
+ * 0 1 2 3 4 5      (4, 5 are children of 2)
+ *   A B C D E
+ *   A B C E D
+ *   A B D C E
+ *   A B D E C
+ *   A B E C D
+ *   A B E D C
+ *   A C B D E
+ *   A C B E D
+ *
+ * 0 1 2 3 4 5
+ *   A A A B B
+ *   A A B A B
+ *   A A B B A
+ */
+public class Exercise_2_4_09 {
+}
---
+++ src/main/java/net/qiguang/algorithms/C2_Sorting/S4_PriorityQueues/Exercise_2_4_11.java
@@ -0,0 +1,12 @@
+package net.qiguang.algorithms.C2_Sorting.S4_PriorityQueues;
+
+/**
+ * 2.4.11 Suppose that your application will have a huge number of insert operations,
+ * but only a few remove the maximum operations. Which priority-queue implementation
+ * do you think would be most effective: heap, unordered array, or ordered array?
+ *
+ * Answer:
+ * Unordered array
+ */
+public class Exercise_2_4_11 {
+}
